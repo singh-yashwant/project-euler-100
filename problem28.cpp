8ff23f372756513d31606938793982f9a6452646
Yashwant Singh <yashwantsinghbtp@gmail.com>
2020-01-12 23:53:09 +0530

    added initial solution for problem28

---
+++ problem28.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<string>
+#include<bits/stdc++.h>
+#include<map>
+
+#include "timer.h"
+
+using namespace std;
+
+int main()
+{
+	ExecutionTimer tmr;
+	vector < vector < int > > spiral;
+	for(int i = 0; i < 1001; i++){
+		vector < int > temp(1001);
+		spiral.push_back(temp);
+	}
+	
+	
+	return 0;
+}
